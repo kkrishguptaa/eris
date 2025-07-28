6ee34291b20387e383523c7ef01577c50c85d6b1
Krish Gupta <m.krishggupta@icloud.com>
2025-07-28 23:39:22 +0530

    prisma

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,9 +1,12 @@
 import { QwikAuth$ } from "@auth/qwik";
 import GitHub from "@auth/qwik/providers/github";
 import Discord from "@auth/qwik/providers/discord";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "~/lib/db";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
     providers: [GitHub, Discord],
-  })
+    adapter: PrismaAdapter(prisma),
+  }),
 );
